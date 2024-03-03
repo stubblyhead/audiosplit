56c0c9b21f37590144eb2b86e581dabe03d9fef7
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2024-03-03 19:22:31 +0000

    this function is kinda fucked, it doesn't reliably split into the requested number of parts but I think it'll do for now?

--- split.py
+++ split.py
@@ -76,7 +76,7 @@ def split_audio_into_chunks(input_file: str, max_chunks: int,
     temp_files = []
     current_chunk_start = 0.0
 
-    selected_items = find_optimal_breakpoints(silence_starts, max_chunks-1)
+    selected_items = find_optimal_breakpoints(silence_starts, max_chunks)
     selected_items.append(duration)
 
     for j in range(0, len(selected_items)):
